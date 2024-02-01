5723af509cbd0797bc023e273d79b364d579fabf
ameralnas <amer.live477@gmail.com>
2024-02-01 04:01:58 +0200

    Now what

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,7 +14,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	theNewNode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 	if (theNewNode == NULL || parent == NULL)
+	{
+		free(theNewNode);
+		theNewNode = NULL;
 		return (NULL);
+	}
 	(*theNewNode).n = value;
 	(*theNewNode).right = NULL;
 	if ((*parent).left != NULL)
