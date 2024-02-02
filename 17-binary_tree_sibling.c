58869e432931c2145e185b354a4c48d0a1f18863
ameralnas <amer.live477@gmail.com>
2024-02-02 23:22:44 +0200

    Try this checker

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - A function to get a sibling
+ * @node: The given node
+ *
+ * Return: A pointer to the sibling
+*/
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if ((*node).parent == NULL || node == NULL)
+		return (NULL);
+	if ((*node).parent != NULL && (*node).n == (*node).parent->left->n)
+	{
+		if ((*node).parent->right != NULL)
+			return ((*node).parent->right);
+		else
+			return (NULL);
+	}
+	else if ((*node).parent != NULL && (*node).n == (*node).parent->right->n)
+	{
+		if ((*node).parent->left != NULL)
+			return ((*node).parent->left);
+		else
+			return (NULL);
+	}
+	else
+		return (NULL);
+}
